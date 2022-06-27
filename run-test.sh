ef7bf2898feb6130c6a4e4a2098909c9b60f859f
Cruz <cruzortiz099@gmail.com>
2022-06-26 20:26:26 -0400

    factory method in go

--- run-test.sh
+++ run-test.sh
@@ -1,3 +1,4 @@
 cd design-pattern-java && ./gradlew test && cd ..
 cd design-pattern-python && ./run-test.sh && cd ..
 cd design-pattern-ts && npm run test && cd ..
+cd design-pattern-go/src && go test ./test/... -v && cd ../..
