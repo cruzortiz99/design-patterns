77910251c9b627554764e4e3ef515d10f0da6cfd
Cruz Ortiz <cruz.ortiz@arivo.com>
2022-07-11 11:22:48 -0400

    adding facade

--- design-pattern-ts/src/patterns/structural/facade/index.ts
+++ design-pattern-ts/src/patterns/structural/facade/index.ts
@@ -12,3 +12,15 @@ export class OggCompressionCodec extends CompressionCode {}
 export class CodecFactory {
   static extract = (file: VideoFile): string => file.filename
 }
+export class Bitrate {
+  static read = (value: string, source: string): string => 
+    "BitrateReader.read: ".concat(value).concat(", ").concat(source)
+  
+  static buffer = (value: string, compression: CompressionCode) =>
+    "BitrateReader.buffer: ".concat(value)
+}
+export class AudioMixer {
+  fix(value: string): string {
+    return "AudioMixer.fix: ".concat(value)
+  }
+}
